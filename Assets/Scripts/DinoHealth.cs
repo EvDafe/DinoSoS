fa53d275bd8c311870189b798daafa689201463c
Artem <evdafe@gmail.com>
2024-01-13 13:46:53 +0300

    +Movement +Animations

---
+++ Assets/Scripts/DinoHealth.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class DinoHealth : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.TryGetComponent(out Obstacle obstacle))
+            {
+                Death();
+            }
+        }
+
+        private void Death()
+        {
+            Debug.Log("Yes");
+        }
+    }
+}
